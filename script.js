05fa64a88c812ebf6a88743ddae81242ec45ea03
insmnia <teamspirit3209@gmail.com>
2022-06-19 01:43:54 +0300

    Interesting facts

--- script.js
+++ script.js
@@ -8,3 +8,22 @@ function slowScroll(id) {
   );
   return false;
 }
+
+function someFact() {}
+
+document.querySelector(".btn-primary").addEventListener("click",() => {
+  let facts = [
+    "Поджег мусорку бытовых отходов в 4 классе, но не спалили",
+    "До 12 лет считал, что если человек носит очки, то у него большие проблемы со здоровьем",
+    "Самый используемый никнейм <i>horekih</i>",
+    "Не ставлю сложные пароли",
+    "Являюсь лидом на проекте в специализации, с который опыта всего 1 месяц"
+  ];
+  new Noty({
+    text: facts[Math.floor(Math.random() * facts.length)],
+    theme: 'mint',
+    layout:'bottomCenter',
+    type:'info',
+    timeout:1000
+  }).show();
+});
